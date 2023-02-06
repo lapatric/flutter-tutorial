39abb0fc8046e28ae3c82cfa577b56f5016c0ace
lapatric <42653152+lapatric@users.noreply.github.com>
2023-02-06 18:21:35 +0100

    Container widget inside Column widget

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -23,7 +23,23 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
         ),
       ),
       body: Column(
-        children: [Image.asset('images/einstein.jpg')],
+        children: [
+          Image.asset('images/einstein.jpg'),
+          const SizedBox(height: 10),
+          const Divider(color: Colors.black),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
+            color: Colors.blueGrey,
+            width: double.infinity,
+            child: const Center(
+              child: Text(
+                'This is a text widget',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
